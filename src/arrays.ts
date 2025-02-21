2fbc1dff958cf5baaf2d2dcd59196484eb98557c
Pokpo <qijia@udel.edu>
2025-02-21 16:19:22 -0500

    makeMath function

--- src/arrays.ts
+++ src/arrays.ts
@@ -105,7 +105,21 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const sum: number = addends.reduce(
+        (curr_sum: number, curr_num: number) => curr_sum + curr_num,
+        0,
+    );
+    const stringRep: string = addends.reduce(
+        (curr_string: string, curr_num: number, curr_index: number) =>
+            curr_index > 0 ?
+                curr_string + "+" + curr_num.toString()
+            :   curr_string + curr_num.toString(),
+        sum.toString() + "=",
+    );
+    return stringRep;
 }
 
 /**
